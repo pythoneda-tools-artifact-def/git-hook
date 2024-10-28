f0ef650bb2a03bd4f7954524ddd03ba26ca5b29c
rydnr <github@acm-sl.org>
2024-10-28 18:53:53 +0100

    Updated nixpkgs

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
   description = "Tool to connect git hooks to PythonEDA through events";
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    nixos.url = "github:NixOS/nixpkgs/23.11";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     pythoneda-shared-artifact-application = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
